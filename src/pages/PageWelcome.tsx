b5226b74099c0f9f1b7c01db42a693ab43a387e3
Edward Tanguay (windows) <edward@tanguay.info>
2024-07-09 10:52:25 +0200

    callback works

--- src/pages/PageWelcome.tsx
+++ src/pages/PageWelcome.tsx
@@ -6,13 +6,14 @@ export const PageWelcome = () => {
 	const [skills, setSkills] = useState<Skill[]>([]);
 
 	useEffect(() => {
-		const _skills = tools.getSkills();
-		console.log(111, _skills);
-		setSkills(_skills);
+		tools.getSkills((_skills) => {
+			setSkills(_skills);
+		});
 	}, []);
+
 	return (
 		<>
 			<h2>There are {skills.length} skills:</h2>
 		</>
-	)
+	);
 };
--- src/tools.ts
+++ src/tools.ts
@@ -1,8 +1,18 @@
 import { Skill } from "./types";
 
-export const getSkills = async (): Skill[] => {
-	const response = await fetch('https://edwardtanguay.vercel.app/share/skills.json');
-	const skills = await response.json();
-	console.log(222, skills);
-	return skills;
+// export const getSkills = async (): Skill[] => {
+// 	const response = await fetch('https://edwardtanguay.vercel.app/share/skills.json');
+// 	const skills = await response.json();
+// 	console.log(222, skills);
+// 	return skills;
+// };
+
+export const getSkills = (callback: (skills: Skill[]) => void): void => {
+	(async () => {
+		const response = await fetch(
+			"https://edwardtanguay.vercel.app/share/skills.json"
+		);
+		const skills = await response.json();
+		callback(skills)
+	})();
 };
