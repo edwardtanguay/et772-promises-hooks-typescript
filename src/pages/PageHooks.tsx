42c41410d5ce84e194389723180c71b5132b2294
Edward Tanguay (windows) <edward@tanguay.info>
2024-07-10 01:50:35 +0200

    fetch finished

--- src/pages/PageHooks.tsx
+++ src/pages/PageHooks.tsx
@@ -11,25 +11,41 @@ export const PageHooks = () => {
 		"https://edwardtanguay.vercel.app/share/skills.json",
 		true
 	);
-	const { items: jobs, isLoading: jobsAreLoading } = useFetch<Job[]>(
-		"nnnhttps://edwardtanguay.vercel.app/share/jobs.json",
+	const {
+		items: jobs,
+		isLoading: jobsAreLoading,
+		hasError: jobsHaveError,
+		error: jobsError,
+	} = useFetch<Job[]>(
+		"https://edwardtanguay.vercel.app/share/jobs.json",
 		true
 	);
 
 	return (
 		<>
 			{skillsHaveError ? (
-				<p className="text-red-800">ERROR: Skills could not load: {skillsError}</p>
+				<p className="text-red-800">
+					ERROR: Skills could not load: {skillsError}
+				</p>
 			) : (
 				<p>
-					There are{" "}
-					{skillsAreLoading ? "(LOADING)" : skills.length} skills.
+					There are {skillsAreLoading ? "(LOADING)" : skills.length}{" "}
+					skills.
 				</p>
 			)}
-			{jobsAreLoading ? (
-				<p>Jobs are loading...</p>
+
+			{jobsHaveError ? (
+				<p className="text-red-800">
+					ERROR: Jobs could not load: {jobsError}
+				</p>
 			) : (
-				<p>There are {jobs.length} jobs.</p>
+				<>
+					{jobsAreLoading ? (
+						<p>Jobs are loading...</p>
+					) : (
+						<p>There are {jobs.length} jobs.</p>
+					)}
+				</>
 			)}
 		</>
 	);
