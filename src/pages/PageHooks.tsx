9a080892fc7d254db80c5f06be0e2d660cb143aa
Edward Tanguay (windows) <edward@tanguay.info>
2024-07-10 01:34:20 +0200

    base useFetch

--- src/pages/PageHooks.tsx
+++ src/pages/PageHooks.tsx
@@ -1,3 +1,24 @@
+import { useFetch } from "../hooks/useFetch";
+import { Skill, Job } from "../types";
+
 export const PageHooks = () => {
-	return <p>hooks</p>;
+	const { items: skills, isLoading: isLoadingSkills } = useFetch<Skill[]>(
+		"https://edwardtanguay.vercel.app/share/skills.json",
+		true
+	);
+	const { items: jobs, isLoading: isLoadingJobs } = useFetch<Job[]>(
+		"https://edwardtanguay.vercel.app/share/jobs.json",
+		true
+	);
+
+	return (
+		<>
+			<p>There are {isLoadingSkills ? "(LOADING...)" : skills.length} skills.</p>
+			{isLoadingJobs ? (
+				<p>Jobs are loading...</p>
+			) : (
+				<p>There are {jobs.length} jobs.</p>
+			)}
+		</>
+	);
 };
