18bda301f854417196e6824c65fc3b1926ee9e6f
Edward Tanguay (windows) <edward@tanguay.info>
2024-07-10 08:14:11 +0200

    about finished

--- src/pages/PageAbout.tsx
+++ src/pages/PageAbout.tsx
@@ -1,5 +1,40 @@
 export const PageAbout = () => {
 	return (
-		<p>This is the about page.</p>
-	)
-}
+		<>
+			<p className="mb-3">
+				This site is a collection of examples I made for a coding
+				challenge, 2024-07-10.
+			</p>
+			<p className="mb-1">For more code examples, see my</p>
+			<ul className="list-disc ml-6">
+				<li>
+					HOWTOS:{" "}
+					<a
+						className="underline"
+						href="https://tanguay-eu.vercel.app/howtos"
+					>
+						https://tanguay-eu.vercel.app/howtos
+					</a>
+				</li>
+				<li>
+					FORAYS:{" "}
+					<a
+						className="underline"
+						href="https://tanguay-eu.vercel.app/forays"
+					>
+						https://tanguay-eu.vercel.app/forays
+					</a>
+				</li>
+				<li>
+					TEMPLATES:{" "}
+					<a
+						className="underline"
+						href="https://tanguay-eu.vercel.app"
+					>
+						https://tanguay-eu.vercel.app
+					</a>
+				</li>
+			</ul>
+		</>
+	);
+};
