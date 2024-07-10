405d3feb66c31c9ebaa6ccb9d3ae25e8306e65f3
Edward Tanguay (windows) <edward@tanguay.info>
2024-07-10 02:49:24 +0200

    overloading

--- src/components/TypeScriptOverloadedFunctionSignatures.tsx
+++ src/components/TypeScriptOverloadedFunctionSignatures.tsx
@@ -1,17 +1,20 @@
 export const TypeScriptOverloadedFunctionSignatures = () => {
-	const displayNames = (nameInfo: string | string[]) => {
+	function displayNameInfo(name: string): string;
+	function displayNameInfo(names: string[]): string;
+
+	function displayNameInfo (nameInfo: string | string[]) {
 		if (typeof nameInfo === "string") {
 			return `Name: ${nameInfo}`;
 		} else if (Array.isArray(nameInfo)) {
 			return `Names: ${nameInfo.join(", ")}`;
 		}
 		throw new Error("Bad type");
-	};
-	
+	}
+
 	return (
 		<>
-			<p>{displayNames("David")}</p>
-			<p>{displayNames(["Jonas", "Angela", "Robert"])}</p>
+			<p>{displayNameInfo("David")}</p>
+			<p>{displayNameInfo(["Jonas", "Angela", "Robert"])}</p>
 		</>
 	);
 };
