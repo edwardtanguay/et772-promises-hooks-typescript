19cdbb4413b1d4e555b17a4d5c9a2e3e2fb1811f
Edward Tanguay (windows) <edward@tanguay.info>
2024-07-10 07:41:45 +0200

    conditional ok

--- src/components/TypeScriptConditionalTypes.tsx
+++ src/components/TypeScriptConditionalTypes.tsx
@@ -1,5 +1,37 @@
-export const TypeScriptConditionalTypes = () => {
-	return (
-		<p>TODO: conditional</p>
-	)
+interface Circle {
+	kind: "circle";
+	radius: number;
 }
+
+interface Square {
+	kind: "square";
+	sideLength: number;
+}
+
+type ShapeProperties<T> = T extends { kind: "circle" }
+	? { radius: number }
+	: T extends { kind: "square" }
+	? { sideLength: number }
+	: never;
+
+// Test cases
+type CircleProps = ShapeProperties<Circle>;
+type SquareProps = ShapeProperties<Square>;
+// type InvalidShapeProps = ShapeProperties<{ kind: "triangle" }>; // is of type never
+
+export const TypeScriptConditionalTypes = () => {
+	const circle: CircleProps = {
+		radius: 5,
+	};
+
+	const square: SquareProps = {
+		sideLength: 7,
+	};
+
+	return (
+		<>
+			<p>Circle has a radius of {circle.radius}.</p>
+			<p>Square has a side length of {square.sideLength}.</p>
+		</>
+	);
+};
