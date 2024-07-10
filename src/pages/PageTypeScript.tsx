f6c0a4751f2a68aee904499fd5b37e1a646601a7
Edward Tanguay (windows) <edward@tanguay.info>
2024-07-10 02:23:39 +0200

    sections

--- src/pages/PageTypeScript.tsx
+++ src/pages/PageTypeScript.tsx
@@ -4,7 +4,14 @@ export const PageTypeScript = () => {
 			<h2 className="appMessage">
 			TypeScript concepts...
 			</h2>
-			<p>typescript</p>
+			<h2 className="section">Overloaded Function Signatures</h2>
+			<p>nnn</p>
+			<h2 className="section mt-3">Conditional Types</h2>
+			<p>nnn</p>
+			<h2 className="section mt-3">Utility Types</h2>
+			<p>nnn</p>
+			<h2 className="section mt-3">Type Guarding</h2>
+			<p>nnn</p>
 		</>
 	);
 };
