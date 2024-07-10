61855eafbc238a85bdd355e6541dbe43a95d07b2
Edward Tanguay (windows) <edward@tanguay.info>
2024-07-10 02:54:43 +0200

    over

--- src/components/TypeScriptOverloadedFunctionSignatures.tsx
+++ src/components/TypeScriptOverloadedFunctionSignatures.tsx
@@ -2,7 +2,7 @@ export const TypeScriptOverloadedFunctionSignatures = () => {
 	function displayNameInfo(name: string): string;
 	function displayNameInfo(names: string[]): string;
 
-	function displayNameInfo (nameInfo: string | string[]) {
+	function displayNameInfo(nameInfo: string | string[]) {
 		if (typeof nameInfo === "string") {
 			return `Name: ${nameInfo}`;
 		} else if (Array.isArray(nameInfo)) {
