deebbe1ad0b2d79a6634ad1a3ed8facc20345b30
Edward Tanguay (windows) <edward@tanguay.info>
2024-07-10 08:20:25 +0200

    about 2

--- src/pages/PageAbout.tsx
+++ src/pages/PageAbout.tsx
@@ -1,16 +1,39 @@
 export const PageAbout = () => {
 	return (
 		<>
-			<p className="mb-3">
+			<p className="mb-1">
 				This site is a collection of examples I made for a coding
 				challenge, 2024-07-10.
 			</p>
-			<p className="mb-1">For more code examples, see my</p>
+			<ul className="list-disc ml-6">
+				<li>
+					REPO:{" "}
+					<a
+						className="underline"
+						target="_blank"
+						href="https://github.com/edwardtanguay/showcase-promises-hooks-typescript"
+					>
+						https://github.com/edwardtanguay/showcase-promises-hooks-typescript
+					</a>
+				</li>
+				<li>
+					NOTES:{" "}
+					<a
+						className="underline"
+						target="_blank"
+						href="https://tanguay-eu.vercel.app/forays/313"
+					>
+					https://tanguay-eu.vercel.app/forays/313
+					</a>
+				</li>
+			</ul>
+			<p className="mb-1 mt-3">For more code examples, see my</p>
 			<ul className="list-disc ml-6">
 				<li>
 					HOWTOS:{" "}
 					<a
 						className="underline"
+						target="_blank"
 						href="https://tanguay-eu.vercel.app/howtos"
 					>
 						https://tanguay-eu.vercel.app/howtos
@@ -20,6 +43,7 @@ export const PageAbout = () => {
 					FORAYS:{" "}
 					<a
 						className="underline"
+						target="_blank"
 						href="https://tanguay-eu.vercel.app/forays"
 					>
 						https://tanguay-eu.vercel.app/forays
@@ -29,6 +53,7 @@ export const PageAbout = () => {
 					TEMPLATES:{" "}
 					<a
 						className="underline"
+						target="_blank"
 						href="https://tanguay-eu.vercel.app"
 					>
 						https://tanguay-eu.vercel.app
